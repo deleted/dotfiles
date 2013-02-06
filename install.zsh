df9923b995252d593f6edc0ced174d9fd02cc262
Ted Scharff <e.b.scharff@nasa.gov>
2013-02-06 07:42:00 -0800

    install script prompts you to make sure you know what you're doing

--- install.zsh
+++ install.zsh
@@ -4,8 +4,10 @@
 # Install symlinks to the files in this repo into the current user's $HOME
 ###
 
-REPO_DIR=$(pwd);
-DEST_DIR=/tmp/home
+#REPO_DIR=$(pwd);
+#DEST_DIR=/tmp/home
+REPO_DIR=$(git rev-parse --show-toplevel)
+DEST_DIR=$HOME
 
 # Don't link this script or the .git subdirectory
 excludes=(
@@ -14,7 +16,15 @@ excludes=(
 )
 # OR-together the subpatterns
 exclude_pattern="\(${(j:\|:)excludes}\)"
-echo EXCLUDE: $exclude_pattern
+#echo EXCLUDE: $exclude_pattern
+
+echo "$REPO_DIR --> $DEST_DIR"
+echo "This will install symlinks to all the regular files in this repository into $DEST_DIR"
+echo "Any existing files will be backed up, with an underscore prefix."
+echo "Afterwards, you probably want to diff any backups to see if there was anything important in them."
+echo "Proceed?  (type okeydoke)"
+read proceed
+[[ $proceed != "okeydoke" ]] && exit 1
 
 for file in $( find -type f $REPO_DIR |grep -v "$exclude_pattern" );
 do;
